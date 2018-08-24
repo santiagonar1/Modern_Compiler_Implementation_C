453524d56f0bc392e28ac10e65c8972286c04088
Santiago Narvaez <santiagonar1@gmail.com>
2018-08-24 12:22:04 +0200

    Add function to count the num elements of a list

--- chap1/program.c
+++ chap1/program.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include "slp.h"
 
+/*
+ * Tells the maximum number of arguments of any print statement within any subexpression of a given
+ * statements
+ */
+int maxargs(A_stm stm);
+
+/*
+ * Tells the number of elements of the list
+ */
+int numargs(A_expList list);
+
 int main(int argc, const char *argv[])
 {
     printf("Running...\n");
@@ -11,6 +22,16 @@ int main(int argc, const char *argv[])
                             A_LastExpList(A_OpExp(A_IdExp("a"), A_minus, A_NumExp(1))))),
                         A_OpExp(A_NumExp(10), A_times, A_IdExp("a")))),
                 A_PrintStm(A_LastExpList(A_IdExp("b")))));
-    printf("Finish...\n");
     return 0;
+}
+
+int maxargs(A_stm stm){
+    return -1;
+}
+
+int numargs(A_expList list){
+    if(list->kind == A_lastExpList)
+        return 1;
+    else
+        return 1 + numargs(list->u.pair.tail);
 }
